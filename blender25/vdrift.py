6bb5194cb283b7f6ac61f336de949b1ed38699d9
logzero <core13@gmx.net>
2011-09-12 10:18:34 +0200

    JPK support. Not all track object properties implemented. Needs testing

--- blender25/vdrift.py
+++ blender25/vdrift.py
@@ -17,10 +17,10 @@
 # ***** END GPL LICENCE BLOCK *****
 
 bl_info = {
-	'name': 'VDrfit JOE format',
+	'name': 'VDrift JOE/JPK format',
 	'description': 'Import-Export to VDrift JOE files (.joe)',
 	'author': 'NaN, port of VDrift blender24 scripts',
-	'version': (0, 6),
+	'version': (0, 7),
 	'blender': (2, 5, 8),
 	'api': 35622,
 	'location': 'File > Import-Export',
@@ -34,6 +34,7 @@ from bpy.props import StringProperty
 from bpy_extras.io_utils import ExportHelper, ImportHelper
 from bpy_extras.image_utils import load_image
 from struct import Struct
+from os import path
 
 class joe_vertex:
 	bstruct = Struct('<fff')
@@ -55,6 +56,7 @@ class joe_vertex:
 			data = joe_vertex.bstruct.pack(v[0], v[1], v[2])
 			file.write(data)
 
+
 class joe_texcoord:
 	bstruct = Struct('<ff')
 	
@@ -75,6 +77,7 @@ class joe_texcoord:
 			data = joe_texcoord.bstruct.pack(v[0], 1 - v[1])
 			file.write(data)
 
+
 class joe_face:
 	__slots__ = 'vertex_index', 'normal_index', 'texture_index'
 	bstruct = Struct('<3h3h3h')
@@ -93,15 +96,17 @@ class joe_face:
 		return self
 	
 	def save(self, file):
-		data = joe_face.bstruct.pack(self.vertex_index[0],self.vertex_index[1],self.vertex_index[2],
+		data = joe_face.bstruct.pack(
+			self.vertex_index[0],self.vertex_index[1],self.vertex_index[2],
 			self.normal_index[0],self.normal_index[1],self.normal_index[2],
 			self.texture_index[0],self.texture_index[1],self.texture_index[2])
 		file.write(data)
 
+
 class joe_frame:
 	__slots__ = 'num_vertices', 'num_normals', 'num_texcoords',\
 				'faces', 'verts', 'texcoords', 'normals'
-	bstruct = Struct("<3i")
+	bstruct = Struct('<3i')
 
 	def __init__(self):
 		self.num_vertices = 0
@@ -234,11 +239,13 @@ class joe_frame:
 		
 		mesh.validate()
 		mesh.update()
-		return bpy.data.objects.new(name, mesh)
+		object = bpy.data.objects.new(name, mesh)
+		bpy.context.scene.objects.link(object)
+		return object
 
 class joe_obj:
 	__slots__ = 'ident', 'version', 'num_faces', 'num_frames', 'frames'
-	bstruct = Struct("<4i")
+	bstruct = Struct('<4i')
 	
 	def __init__(self):
 		self.ident = 844121161
@@ -289,12 +296,12 @@ class joe_obj:
 		self.num_frames = num_frames
 		self.num_faces = len(self.frames[0].faces)
 		return self
-'''
+
+
 class joe_pack:
 	versionstr = 'JPK01.00'
-	binary_format = '<2i'  #little-endian (<), 2 integers (2i)
-	binary_size = struct.calcsize(binary_format)
-
+	bstruct = Struct('<2i')
+	
 	def __init__(self):
 		self.numobjs = 0
 		self.maxstrlen = 0
@@ -304,80 +311,107 @@ class joe_pack:
 		self.surfaces = []
 			
 	def load(self, filename):
+		from time import clock
 		# don't change call order
+		t0 = clock()
 		self.load_list(filename)
+		t1 = clock()
+		print('load list: ' + str(t1 - t0))
+		t0 = t1
 		self.load_images(filename)
+		t1 = clock()
+		print('load images: ' + str(t1 - t0))
+		t0 = t1
 		self.load_jpk(filename)
+		t1 = clock()
+		print('load jpk: ' + str(t1 - t0))
 		return self
 	
 	def save(self, filename):
+		from time import clock
+		t0 = clock()
 		self.save_jpk(filename)
+		t1 = clock()
+		print('save jpk: ' + str(t1 - t0))
+		t0 = t1
 		self.save_list(filename)
+		t1 = clock()
+		print('save list: ' + str(t1 - t0))
 		
 	def to_mesh(self):
 		trackobject_to_obj_init()
-		for name, joe in self.joe.iteritems():
+		for name, joe in self.joe.items():
 			image = None
 			trackobject = self.list.get(name)
 			if trackobject:
 				imagename = trackobject.values[1]
 				image = self.images[imagename]
 				obj = joe.to_mesh(name, image)
-				me = obj.getData(mesh=1)
 				trackobject.to_obj(obj)
 			else:
-				print name + ' not in list.txt.'
+				print(name + ' not imported. Not in list.txt.')
 		
 	def from_mesh(self):
-		objlist = Blender.Object.Get()
+		objlist = bpy.context.scene.objects
 		trackobject_from_obj_init()
 		for obj in objlist:
-			if obj.getType() == 'Mesh' and not obj.name.startswith('~temp')\
-			and len(obj.getData().faces) > 0 and obj.getData().faces[0].image is not None:
-				objname = obj.name
-				trackobj = trackobject().from_obj(obj)
-				# override obj name
-				if len(trackobj.values[0]):
-					objname = trackobj.values[0]
-				# loader expects a joe file
-				if not objname.endswith('.joe'):
-					objname = objname + '.joe'
-					trackobj.values[0] = objname
-				self.list[objname] = trackobj
-				self.joe[objname] = joe_obj().from_mesh(obj)
-				self.maxstrlen = max(self.maxstrlen, len(objname))
-			else:
-				mesh = obj.getData(mesh=True)
-				if 'joename' in mesh.properties:
-					print mesh.properties['joename'] + ' was not exported.'
-				else:
-					print obj.name + ' was not exported.'
+			if obj.type != 'MESH':
+				continue
+			if obj.name.startswith('~'):
+				continue
+			if len(obj.data.faces) == 0:
+				print(obj.name + ' not exported. No faces.')
+				continue
+			if len(obj.data.uv_textures) == 0:
+				print(obj.name + ' not exported. No texture coordinates.')
+				continue
+			if obj.data.uv_textures[0].data[0].image == None:
+				print(obj.name + ' not exported. No texture assigned.')
+				continue
+			objname = obj.name
+			trackobj = trackobject().from_obj(obj)
+			# override obj name
+			if len(trackobj.values[0]):
+				objname = trackobj.values[0]
+			# loader expects a joe file
+			if not objname.endswith('.joe'):
+				objname = objname + '.joe'
+				trackobj.values[0] = objname
+			self.list[objname] = trackobj
+			self.joe[objname] = joe_obj().from_mesh(obj)
+			self.maxstrlen = max(self.maxstrlen, len(objname))
 		self.numobjs = len(self.joe)
 		return self
 		
 	def load_jpk(self, filename):
 		file = open(filename, 'rb')
 		# header
-		version = file.read(len(self.versionstr))
-		temp = file.read(self.binary_size)
-		data = struct.unpack(self.binary_format, temp)
-		self.numobjs = data[0]
-		self.maxstrlen = data[1]
+		version = file.read(len(joe_pack.versionstr))
+		data = file.read(joe_pack.bstruct.size)
+		v = joe_pack.bstruct.unpack(data)
+		self.numobjs = v[0]
+		self.maxstrlen = v[1]
 		# fat
 		fat = []
 		for i in range(self.numobjs):
-			temp = file.read(self.binary_size)
-			data = struct.unpack(self.binary_format, temp)
-			offset = data[0]
-			length = data[1]
-			name = file.read(self.maxstrlen)
-			fat.append((offset, length, util.stripz(name)))
+			data = file.read(joe_pack.bstruct.size)
+			v = joe_pack.bstruct.unpack(data)
+			offset = v[0]
+			length = v[1]
+			data = file.read(self.maxstrlen)
+			# strip trailing zeros
+			for i in range(self.maxstrlen):
+				if data[i] == 0:
+					data = data[:i]
+					break
+			name = data.decode('ascii')
+			fat.append((offset, length, name))
 		# data
 		for offset, length, name in fat:
 			pos = file.tell()
 			delta = offset - pos
 			if delta < 0:
-				print 'Error reading: ', name, offset
+				print('Error reading: ', name, offset)
 				return
 			elif delta > 0:
 				file.read(delta)
@@ -386,23 +420,24 @@ class joe_pack:
 		file.close()
 	
 	def save_jpk(self, filename):
-		if not Blender.sys.exists(filename):
-			open(filename, 'w').close()
-		file = open(filename, 'rb+')
+		try:
+			file = open(filename, 'rb+')
+		except IOError:
+			file = open(filename, 'wb')
 		# header
-		file.write(self.versionstr)
-		data = struct.pack(self.binary_format, self.numobjs, self.maxstrlen)
+		file.write(self.versionstr.encode('ascii'))
+		data = joe_pack.bstruct.pack(self.numobjs, self.maxstrlen)
 		file.write(data)
 		# allocate fat
 		fat_offset = file.tell()
 		for i in range(self.numobjs):
-			data = struct.pack(self.binary_format, 0, 0)
+			data = joe_pack.bstruct.pack(0, 0)
 			file.write(data)
 			name = util.fillz('', self.maxstrlen)
-			file.write(name)
+			file.write(name.encode('ascii'))
 		# write data / build fat
 		fat = []
-		for name, joe in self.joe.iteritems():
+		for name, joe in self.joe.items():
 			offset = file.tell()
 			joe.save(file)
 			length = file.tell() - offset
@@ -410,70 +445,71 @@ class joe_pack:
 		# fill fat
 		file.seek(fat_offset)
 		for offset, length, name in fat:
-			data = struct.pack(self.binary_format, offset, length)
+			data = joe_pack.bstruct.pack(offset, length)
 			file.write(data)
 			name = util.fillz(name, self.maxstrlen)
-			file.write(name)
+			file.write(name.encode('ascii'))
 		file.close()
 		
 	def load_list(self, filename):
-		dir = Blender.sys.dirname(filename)
-		list_path = Blender.sys.join(dir, 'list.txt')
-		#debug = open(Blender.sys.join(dir, 'debug.txt'), 'w')
-		if not Blender.sys.exists(list_path):
-			print list.path + ' not found.'
+		dir = path.dirname(filename)
+		list_path = path.join(dir, 'list.txt')
+		try:
+			list_file = open(list_path)
+		except IOError:
+			print(list_path + ' not found.')
 			return
 		# read objects
-		list_file = open(list_path)
 		line = list_file.readline()
 		while line != '':
 			if '.joe' in line:
 				object = trackobject()
 				name = line.strip()
 				line = object.read(name, list_file)
-				#debug.write(str(object.values)+'\n')
 				self.list[object.values[0]] = object
 			else:
 				line = list_file.readline()
 		if len(self.list) == 0:
-			print 'Failed to load list.txt.'
+			print('Failed to load list.txt.')
 		list_file.close()
-		#debug.close()
 		
 	def save_list(self, filename):
-		dir = Blender.sys.dirname(filename)
-		list_path = Blender.sys.join(dir, 'list.txt')
+		dir = path.dirname(filename)
+		list_path = path.join(dir, 'list.txt')
 		file = open(list_path, 'w')
 		file.write('17\n\n')
 		i = 0
-		for name, object in self.list.iteritems():
+		for name, object in self.list.items():
 			file.write('#entry ' + str(i) + '\n')
 			object.write(file)
 			i = i + 1
 		file.close()
 		
 	def load_images(self, filename):
-		dir = Blender.sys.dirname(filename)
-		for name, object in self.list.iteritems():
+		dir = path.dirname(filename)
+		for name, object in self.list.items():
 			imagename = object.values[1]
 			if imagename not in self.images:
-				path = Blender.sys.join(dir, imagename)
-				self.images[imagename] = util.load_image(path)
+				imagepath = path.join(dir, imagename)
+				self.images[imagename] = load_image(imagepath)
+
 
 def trackobject_to_obj_init():
 	trackobject.grp_surf = []
 	trackobject.grp = {}
 	for name in ('mipmap', 'nolighting', 'skybox', 'transparent', 'doublesided', 'collidable', 'shadow'):
-		try: trackobject.grp[name] = Group.Get(name).objects
-		except: trackobject.grp[name] = Group.New(name).objects
+		grp = bpy.data.groups.get(name)
+		if grp == None:
+			grp = bpy.data.groups.new(name)
+		trackobject.grp[name] = grp.objects
+
 
 def trackobject_from_obj_init():
 	trackobject_to_obj_init()
 	trackobject.is_surf = []
-	for grp in Group.Get():
+	for grp in bpy.data.groups:
 		if grp.name.startswith('surface'):
 			trackobject.is_surf.append((grp.name.split('-')[-1], set(grp.objects)))
-			print grp.name
 		elif grp.name == 'mipmap':
 			trackobject.is_mipmap = set(grp.objects)
 		elif grp.name == 'nolighting':
@@ -490,7 +526,10 @@ def trackobject_from_obj_init():
 			trackobject.is_doublesided = set(grp.objects)
 
 class trackobject:
-	names = ['model', 'texture', 'mipmap', 'lighting', 'skybox', 'blend', 'bump length', 'bump amplitude', 'drivable', 'collidable', 'non treaded', 'treaded', 'roll resistance', 'roll drag', 'shadow', 'clamp', 'surface']
+	names = ('model', 'texture', 'mipmap', 'lighting', 'skybox', 'blend',\
+			'bump length', 'bump amplitude', 'drivable', 'collidable',\
+			'non treaded', 'treaded', 'roll resistance', 'roll drag',\
+			'shadow', 'clamp', 'surface')
 	namemap = dict(zip(names, range(17)))
 	
 	def __init__(self):
@@ -515,56 +554,46 @@ class trackobject:
 			list_file.write(v + '\n')
 		list_file.write('\n')
 	
-	def to_obj(self, sceneobject):
-		# set properties (deprecated)
-		for name, value in zip(trackobject.names, self.values):
-			sceneobject.addProperty(name, value, 'STRING')
-		# set group
-		if self.values[2] == '1': trackobject.grp['mipmap'].link(sceneobject)
-		if self.values[3] == '1': trackobject.grp['nolighting'].link(sceneobject)
-		if self.values[4] == '1': trackobject.grp['skybox'].link(sceneobject)
-		if self.values[5] == '1': trackobject.grp['transparent'].link(sceneobject)
-		elif self.values[5] == '2': trackobject.grp['doublesided'].link(sceneobject)
-		if self.values[8] == '1' or self.values[9] == '1': trackobject.grp['collidable'].link(sceneobject)
-		if self.values[14] == '1': trackobject.grp['shadow'].link(sceneobject)
+	def to_obj(self, object):
+		object['model'] = self.values[0]
+		object['texture'] = self.values[1]
+		if self.values[2] == '1': trackobject.grp['mipmap'].link(object)
+		if self.values[3] == '1': trackobject.grp['nolighting'].link(object)
+		if self.values[4] == '1': trackobject.grp['skybox'].link(object)
+		if self.values[5] == '1': trackobject.grp['transparent'].link(object)
+		elif self.values[5] == '2': trackobject.grp['doublesided'].link(object)
+		if self.values[8] == '1' or self.values[9] == '1': trackobject.grp['collidable'].link(object)
+		if self.values[14] == '1': trackobject.grp['shadow'].link(object)
 		surfid = int(self.values[16])
 		while surfid >= len(trackobject.grp_surf):
 			surfnum = len(trackobject.grp_surf)
 			surfname = 'surface-'+str(surfnum)
-			try: grp = Group.Get(surfname).objects
-			except: grp = Group.New(surfname).objects
-			trackobject.grp_surf.append(grp)
-		trackobject.grp_surf[surfid].link(sceneobject)
+			grp = bpy.data.groups.get(surfname)
+			if grp == None:
+				grp = bpy.data.groups.new(surfname)
+			trackobject.grp_surf.append(grp.objects)
+		trackobject.grp_surf[surfid].link(object)
 		return self
 	
 	# set from object
-	def from_obj(self, sceneobject):
-		# get ptoperties (deprecated)
-		properties = sceneobject.getAllProperties()
-		for p in properties:
-			if p.name in trackobject.namemap:
-				self.values[trackobject.namemap[p.name]] = p.data
-		# if texture, mesh properties not set, get them from mesh
-		if self.values[0] == 'none':
-			self.values[0] = sceneobject.name
-		if self.values[1] == 'none':
-			self.values[1] = sceneobject.getData().faces[0].image.getName()
-		# get group properties
-		self.values[2] = '1' if sceneobject in trackobject.is_mipmap else '0'
-		self.values[3] = '1' if sceneobject in trackobject.is_nolighting else '0'
-		self.values[4] = '1' if sceneobject in trackobject.is_skybox else '0'
-		self.values[9] = '1' if sceneobject in trackobject.is_collidable else '0'
-		self.values[14] = '1' if sceneobject in trackobject.is_shadow else '0'
-		if sceneobject in trackobject.is_transparent: self.values[5] = '1'
-		elif sceneobject in trackobject.is_doublesided: self.values[5] = '2'
+	def from_obj(self, object):
+		self.values[0] = object.get('model', object.name)
+		self.values[1] = object.get('texture', object.data.uv_textures[0].data[0].image.name)
+		self.values[2] = '1' if object in trackobject.is_mipmap else '0'
+		self.values[3] = '1' if object in trackobject.is_nolighting else '0'
+		self.values[4] = '1' if object in trackobject.is_skybox else '0'
+		self.values[9] = '1' if object in trackobject.is_collidable else '0'
+		self.values[14] = '1' if object in trackobject.is_shadow else '0'
+		if object in trackobject.is_transparent: self.values[5] = '1'
+		elif object in trackobject.is_doublesided: self.values[5] = '2'
 		else: self.values[5] = '0'
 		for name, grp in self.is_surf:
-			if sceneobject in grp:
+			if object in grp:
 				self.values[16] = name
-				print 'grp' + name
 				break
 		return self
-'''
+
+
 class util:
 	# helper class to filter duplicates
 	class indexed_set(object):
@@ -581,14 +610,6 @@ class util:
 			else:
 				ni = self.map[fixed]
 			return ni
-
-	# strip trailing zeroes
-	@staticmethod
-	def stripz(s):
-		n = 0
-		while (n < len(s) and ord(s[n]) != 0):
-			n = n + 1
-		return s[0:n]
 
 	# fill trailing zeroes
 	@staticmethod
@@ -642,6 +663,7 @@ class util:
 			util.delete_object(object)
 		return mesh
 
+
 class export_joe(bpy.types.Operator, ExportHelper):
 	bl_idname = 'export.joe'
 	bl_label = 'Export JOE'
@@ -655,12 +677,10 @@ class export_joe(bpy.types.Operator, ExportHelper):
 			self.object = bpy.context.selected_objects[0]
 		except:
 			self.object = None
-		#bpy.ops.object.mode_set(mode='OBJECT', toggle = False)
 	
 	def execute(self, context):
 		props = self.properties
-		filepath = self.filepath
-		filepath = bpy.path.ensure_ext(filepath, self.filename_ext)
+		filepath = bpy.path.ensure_ext(self.filepath, self.filename_ext)
 		
 		if len(bpy.context.selected_objects[:]) == 0:
 			raise NameError('Please select one object!')
@@ -686,6 +706,7 @@ class export_joe(bpy.types.Operator, ExportHelper):
 		context.window_manager.fileselect_add(self);
 		return {'RUNNING_MODAL'}
 
+
 class import_joe(bpy.types.Operator, ImportHelper):
 	bl_idname = 'import.joe'
 	bl_label = 'Import JOE'
@@ -701,28 +722,86 @@ class import_joe(bpy.types.Operator, ImportHelper):
 			image = None #load_image(filepath_img)
 			file = open(filepath, 'rb')
 			joe = joe_obj().load(file)
+			joe.to_mesh(bpy.path.basename(filepath), image)
 			file.close()
-			object = joe.to_mesh(bpy.path.basename(filepath), image)
-			context.scene.objects.link(object)
+		finally:
+			self.report({'INFO'},  filepath + ' imported')
+		return {'FINISHED'}
+
+
+class export_jpk(bpy.types.Operator, ExportHelper):
+	bl_idname = 'export.jpk'
+	bl_label = 'Export JPK'
+	filename_ext = '.jpk'
+	filter_glob = StringProperty(
+			default='*.jpk',
+			options={'HIDDEN'})
+	
+	def execute(self, context):
+		props = self.properties
+		filepath = bpy.path.ensure_ext(self.filepath, self.filename_ext)
+		try:
+			jpk = joe_pack().from_mesh()
+			jpk.save(filepath)
+		finally:
+			self.report({'INFO'},  filepath + ' exported')
+		
+		return {'FINISHED'}
+		
+	def invoke(self, context, event):
+		context.window_manager.fileselect_add(self);
+		return {'RUNNING_MODAL'}
+
+
+class import_jpk(bpy.types.Operator, ImportHelper):
+	bl_idname = 'import.jpk'
+	bl_label = 'Import JPK'
+	filename_ext = '.jpk'
+	filter_glob = StringProperty(
+		default='*.jpk',
+		options={'HIDDEN'})
+	
+	def execute(self, context):
+		props = self.properties
+		filepath = bpy.path.ensure_ext(self.filepath, self.filename_ext)
+		try:
+			jpk = joe_pack().load(filepath)
+			jpk.to_mesh()
 		finally:
 			self.report({'INFO'},  filepath + ' imported')
 		return {'FINISHED'}
 
 def menu_export_joe(self, context):
 	self.layout.operator(export_joe.bl_idname, text = 'VDrift JOE (.joe)')
-	
+
+
 def menu_import_joe(self, context):
 	self.layout.operator(import_joe.bl_idname, text = 'VDrift JOE (.joe)')
- 
+
+
+def menu_export_jpk(self, context):
+	self.layout.operator(export_jpk.bl_idname, text = 'VDrift JPK (.jpk)')
+
+
+def menu_import_jpk(self, context):
+	self.layout.operator(import_jpk.bl_idname, text = 'VDrift JPK (.jpk)')
+
+
 def register():
 	bpy.utils.register_module(__name__)
 	bpy.types.INFO_MT_file_export.append(menu_export_joe)
 	bpy.types.INFO_MT_file_import.append(menu_import_joe)
- 
+	bpy.types.INFO_MT_file_export.append(menu_export_jpk)
+	bpy.types.INFO_MT_file_import.append(menu_import_jpk)
+
+
 def unregister():
 	bpy.utils.unregister_module(__name__)
 	bpy.types.INFO_MT_file_export.remove(menu_export_joe)
 	bpy.types.INFO_MT_file_import.remove(menu_import_joe)
- 
+	bpy.types.INFO_MT_file_export.remove(menu_export_jpk)
+	bpy.types.INFO_MT_file_import.remove(menu_import_jpk)
+
+
 if __name__ == '__main__':
 	register()
